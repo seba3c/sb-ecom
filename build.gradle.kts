f0f6cd066592b45db0c4cef776624cc82871d631
Sebastian Castañeda <castanedacs@gmail.com>
2026-03-02 12:11:46 +0100

    Initial project layout

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.ecommerce"
+version = "0.0.1-SNAPSHOT"
+description = "sb-ecom"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
