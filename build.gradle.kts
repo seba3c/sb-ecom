619a2fde4b18381d958912f1e50ddf9067774671
Sebastian Castañeda <castanedacs@gmail.com>
2026-03-06 19:30:13 +0100

    Minor fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,10 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-h2console")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    "developmentOnly"("org.springframework.boot:spring-boot-devtools")
-    implementation("com.h2database:h2")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
