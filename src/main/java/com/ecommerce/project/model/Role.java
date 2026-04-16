a46d080374254e248d68ea03ec19e41126bae023
Sebastian Castañeda <castanedacs@gmail.com>
2026-04-16 11:03:15 +0200

    Minor fixes

--- src/main/java/com/ecommerce/project/model/Role.java
+++ src/main/java/com/ecommerce/project/model/Role.java
@@ -15,7 +15,7 @@ public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Enumerated(EnumType.STRING)
     @Column(length = 20)
--- src/test/java/com/ecommerce/project/security/jwt/JwtAuthEntryPointTest.java
+++ src/test/java/com/ecommerce/project/security/jwt/JwtAuthEntryPointTest.java
@@ -1,6 +1,6 @@
 package com.ecommerce.project.security.jwt;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
