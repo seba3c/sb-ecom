67dce72f8287146432fd1a3ec236a8452a6a9bb5
Sebastian Castañeda <castanedacs@gmail.com>
2026-04-24 17:36:48 +0200

    Added docker compose for MySQL and postgree dbs

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     testImplementation("com.fasterxml.jackson.core:jackson-databind")
