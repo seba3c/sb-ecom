b7c2fa6a50abedbfae2e697394933e23caea54b6
Sebastian Castañeda <castanedacs@gmail.com>
2026-04-25 02:34:30 +0200

    Added profiles for dbs

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 dependencies {
     annotationProcessor("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     implementation("com.fasterxml.jackson.core:jackson-databind")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     implementation("org.modelmapper:modelmapper:3.2.6")
